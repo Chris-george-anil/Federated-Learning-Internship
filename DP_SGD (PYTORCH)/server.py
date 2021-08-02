e25c18acc8f86cfc94da00642bddbd8ea3ce72d2
Chris-george-anil <66996004+Chris-george-anil@users.noreply.github.com>
2021-08-02 21:58:57 +0530

    Add files via upload

---
+++ DP_SGD (PYTORCH)/server.py
@@ -0,0 +1,165 @@
+# # # Start Flower server for three rounds of federated learning
+# fl.server.start_server("localhost:3000",config={"num_rounds": 3})
+import flwr as fl
+from pandas.core.arrays.integer import Int64Dtype
+from typing import Any, Callable, Dict, List, Optional, Tuple
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.engine.training import Model
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms 
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+from torchvision.datasets import CIFAR10
+import numpy as np
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+import matplotlib.pyplot as plt 
+from opacus import PrivacyEngine
+from torch.autograd import Variable
+from collections import OrderedDict
+
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+acc=[]
+tot_loss=[]
+num_workers = 0
+# how many samples per batch to load
+batch_size = 20
+# percentage of training set to use as validation
+valid_size = 0.2
+
+# convert data to a normalized torch.FloatTensor
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5) )
+    ])
+
+# choose the training and test datasets
+train_data = datasets.MNIST('data', train=True,
+                              download=True, transform=transform)
+test_data = datasets.MNIST('data', train=False,
+                             download=True, transform=transform)
+
+# obtain training indices that will be used for validation
+num_train = len(train_data)
+indices = list(range(num_train))
+np.random.shuffle(indices)
+split = int(np.floor(valid_size * num_train))
+train_idx, valid_idx = indices[split:], indices[:split]
+
+# define samplers for obtaining training and validation batches
+train_sampler = SubsetRandomSampler(train_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
+
+# prepare data loaders (combine dataset and sampler)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+    sampler=train_sampler, num_workers=num_workers)
+valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, 
+    sampler=valid_sampler, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, 
+    num_workers=num_workers)
+
+# specify the image classes
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+def set_parameters(x,parameters):
+        params_dict = zip(x.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+        x.load_state_dict(state_dict, strict=True)
+
+def get_eval_fn(model):
+    """Return an evaluation function for server-side evaluation."""
+    # Use the last 5k training examples as a validation set
+    # The `evaluate` function will be called after every round
+    def evaluate(weights: fl.common.Weights):
+        """Validate the network on the entire test set."""
+        criterion = torch.nn.CrossEntropyLoss()
+        correct, total, loss = 0, 0, 0.0
+        set_parameters(model,weights)  # Update model with the latest parameters
+        with torch.no_grad():
+            for data in valid_loader:
+                images, labels = data[0].to(DEVICE), data[1].to(DEVICE)
+                outputs = model(images)
+                loss += criterion(outputs, labels).item()
+                test_output = model(images)
+                pred_y = torch.max(test_output, 1)[1].data.squeeze()
+                accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+        print(accuracy)
+        acc.append(accuracy)
+        return loss, {"accuracy": accuracy}
+
+    return evaluate
+
+
+def fit_config(rnd: int):
+    """Return training configuration dict for each round.
+    Keep batch size fixed at 32, perform two rounds of training with one
+    local epoch, increase to two local epochs afterwards.
+    """
+    config = {
+        "batch_size": 32,
+        "local_epochs": 1 if rnd < 2 else 2,
+    }
+    return config
+
+def evaluate_config(rnd: int):
+    """Return evaluation configuration dict for each round.
+    Perform five local evaluation steps on each client (i.e., use five
+    batches) during rounds one to three, then increase to ten local
+    evaluation steps.
+    """
+    val_steps = 5 if rnd < 4 else 10
+    return {"val_steps": val_steps}
+
+class Net(nn.Module):
+    def __init__(self) -> None:
+      super(Net, self).__init__()
+      self.conv1 = nn.Sequential(         
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=16,            
+                kernel_size=5,              
+                stride=1,                   
+                padding=2,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2), )
+      self.conv2 = nn.Sequential(         
+            nn.Conv2d(16, 32, 5, 1, 2),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),                
+        )
+        # fully connected layer, output 10 classes
+      self.out = nn.Linear(32 * 7 * 7, 10)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)       
+        output = self.out(x)
+        return output
+
+model=Net()
+def get_parameters(model):
+        return [val.cpu().numpy() for _, val in model.state_dict().items()]
+
+strategy = fl.server.strategy.FedAvg(
+        fraction_fit=0.3,
+        fraction_eval=0.2,
+        min_fit_clients=2,
+        min_eval_clients=2,
+        min_available_clients=2,
+        eval_fn=get_eval_fn(model),
+        on_fit_config_fn=fit_config,
+        on_evaluate_config_fn=evaluate_config,
+        initial_parameters=fl.common.weights_to_parameters(get_parameters(model)))
+def start(strategy):
+    fl.server.start_server("localhost:3000", config={"num_rounds": 3},strategy=strategy)
+   
+start(strategy)
+print(acc)

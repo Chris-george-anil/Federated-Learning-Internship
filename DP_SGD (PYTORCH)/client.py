6eaf7bbdae4a20e95e98f3f9fdaeb72c34b2cf0b
Chris-george-anil <66996004+Chris-george-anil@users.noreply.github.com>
2021-08-02 21:57:27 +0530

    Create client.py

---
+++ DP_SGD (PYTORCH)/client.py
@@ -0,0 +1,167 @@
+from collections import OrderedDict
+import torch
+from torch import optim
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+from torch.autograd import Variable
+import flwr as fl
+import numpy as np
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+import matplotlib.pyplot as plt 
+from opacus import PrivacyEngine
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+acc=[]
+privacy=0
+num_workers = 0
+# how many samples per batch to load
+batch_size = 32
+# percentage of training set to use as validation
+valid_size = 0.2
+
+# convert data to a normalized torch.FloatTensor
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+# choose the training and test datasets
+train_data = datasets.MNIST('data', train=True,
+                              download=True, transform=transform)
+test_data = datasets.MNIST('data', train=False,
+                             download=True, transform=transform)
+
+# obtain training indices that will be used for validation
+num_train = len(train_data)
+indices = list(range(num_train))
+np.random.shuffle(indices)
+split = int(np.floor(valid_size * num_train))
+train_idx, valid_idx = indices[split:], indices[:split]
+
+# define samplers for obtaining training and validation batches
+train_sampler = SubsetRandomSampler(train_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
+
+# prepare data loaders (combine dataset and sampler)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+    sampler=train_sampler, num_workers=num_workers)
+valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, 
+    sampler=valid_sampler, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, 
+    num_workers=num_workers)
+
+# specify the image classes
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+
+
+
+
+def train(net,optimizer, trainloader, epochs):
+    """Train the network on the training set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    # optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    # privacy_engine = PrivacyEngine(net, sample_rate=0.01, noise_multiplier=1.3, max_grad_norm=1.0)
+    # privacy_engine.attach(optimizer)
+    net.train()
+    for _ in range(epochs):
+        for images, labels in trainloader:
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+            optimizer.zero_grad()
+            b_x = Variable(images)   # batch x
+            b_y = Variable(labels)
+            loss = criterion(net(images), labels)
+            loss.backward()
+            optimizer.step()
+
+def test(net, testloader):
+    """Validate the network on the entire test set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, total, loss = 0, 0, 0.0
+    net.eval()
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data[0].to(DEVICE), data[1].to(DEVICE)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            test_output = net(images)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+
+
+            # _, predicted = torch.max(outputs.data, 1)
+            # total += labels.size(0)
+            # correct += (predicted == labels).sum().item()
+    # accuracy = correct / total
+    return loss, accuracy
+
+class Net(nn.Module):
+    def __init__(self) -> None:
+        super(Net, self).__init__()
+        self.conv1 = nn.Sequential(         
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=16,            
+                kernel_size=5,              
+                stride=1,                   
+                padding=2,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2), )
+        self.conv2 = nn.Sequential(         
+            nn.Conv2d(16, 32, 5, 1, 2),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),                
+        )
+        # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 7 * 7, 10)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)       
+        output = self.out(x)
+        return output
+
+# Load model and data
+
+class MNISTClient(fl.client.NumPyClient):
+    def __init__(self,mod):
+        self.net = mod
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+        ## DP-SGD Implementation
+        self.optimizer = torch.optim.SGD(mod.parameters(), lr=0.001, momentum=0.9)
+        self.privacy_engine = PrivacyEngine(mod, sample_rate=batch_size/num_train,sample_size=num_train, noise_multiplier=0.0, max_grad_norm=1.0)
+        self.privacy_engine.attach(self.optimizer)
+
+    def get_parameters(self):
+        return [val.cpu().numpy() for _, val in self.net.state_dict().items()]
+
+    def set_parameters(self, parameters):
+        params_dict = zip(self.net.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+        self.net.load_state_dict(state_dict, strict=True)
+
+    def fit(self, parameters, config):
+        self.set_parameters(parameters)
+        train(self.net,self.optimizer, train_loader, epochs=1)
+        global privacy
+        privacy+=self.privacy_engine.get_privacy_spent(10**(-5))[0]
+        return self.get_parameters(), len(train_loader), {}
+
+    def evaluate(self, parameters, config):
+        self.set_parameters(parameters)
+        loss, accuracy = test(self.net, test_loader)
+        acc.append(accuracy)
+
+        return float(loss), len(test_loader), {"accuracy":float(accuracy)}
+
+fl.client.start_numpy_client("localhost:3000", client=MNISTClient(Net()))
+print("Final Acc",acc)
+print("Privacy Loss",privacy)
